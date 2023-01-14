6b168632de3102cc93d6ee86367bb6cc37533dbe
YurecUA <81627173+YurecUA@users.noreply.github.com>
2023-01-14 06:08:30 +0200

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,69 +1,53 @@
-import { lazy, Suspense } from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
-import { useQuery } from 'react-query';
-import * as MoviesAPI from '../services/MoviesApi';
+import { useState, useEffect } from 'react';
+import { getTrendingMovie } from '../services/MoviesApi';
+import { Link, useLocation } from 'react-router-dom';
 
-import Preloader from '../components/Preloader/Preloader';
-import NotFound from '../components/NotFound/NotFound';
-
-const MoviesList = lazy(() =>
-  import('../components/MoviesList'),
-);
-
-const PaginationList = lazy(
-  () =>
-    import('../components/PaginationList'),
-);
-
-export default function HomePage() {
-  const history = useHistory();
+const HomePage = () => {
   const location = useLocation();
-  const page = new URLSearchParams(location.search).get('page') ?? 1;
+  const [movies, setMovies] = useState([]);
 
-  const { data, isLoading, isError, isFetching } = useQuery(
-    ['movies', page],
-    () => MoviesAPI.fetchTrendingMoviesByPage(page),
-    { keepPreviousData: true },
-  );
+  useEffect(() => {
+    const getMovies = async () => {
+      const { results } = await getTrendingMovie();
 
-  const handleChange = (event, value) => {
-    const options = {
-      top: 0,
-      behavior: 'smooth',
+      setMovies(results);
     };
 
-    window.scrollTo(options);
-    history.push({ ...location, search: `page=${value}` });
-  };
-
-  if (isLoading || isFetching) {
-    return (
-      <>
-        <Preloader />
-      </>
-    );
-  }
-
-  if (isError) {
-    return (
-      <>
-        <NotFound text={'Nothing was found, please try again'} />
-      </>
-    );
-  }
+    getMovies();
+  }, []);
 
   return (
-    <div>
-      <Suspense fallback={<Preloader />}>
-        <MoviesList movies={data.results} url="" />
+    <>
+      <h1>Trending today</h1>
 
-        <PaginationList
-          movies={data.results}
-          totalPages={data.total_pages}
-          page={Number(page)}
-          handleChange={handleChange}
-        />
-      </Suspense>
-    </div>
+      {movies &&
+        movies.map(
+          ({
+            id,
+            title,
+
+          }) => (
+            <ul>
+              <li key={id}>
+                <Link
+                  to={{
+                    pathname: `/movies/${`${id}`}`,
+                    state: {
+                      from: {
+                        location,
+                        label: 'Back to Home',
+                      },
+                    },
+                  }}
+                >
+                  <p>{title}</p>
+                </Link>
+              </li>
+            </ul>
+          )
+        )}
+    </>
   );
-}
+};
+
+export default HomePage;
