04577f32cbe062f76c29e0d5807d1a7bf48dbbb1
YurecUA <81627173+YurecUA@users.noreply.github.com>
2023-01-14 02:26:54 +0200

    Create MoviesPage.jsx

---
+++ src/pages/MoviesPage.jsx
@@ -0,0 +1,86 @@
+import { useState, useEffect, useRef, lazy, Suspense } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import SearchForm from '../components/SearchForm/SearchForm';
+import * as MoviesAPI from '../services/MoviesApi';
+import Preloader from '../components/Preloader/Preloader';
+
+const MoviesList = lazy(() =>
+  import('../components/MoviesList/MoviesList'),
+);
+
+const PaginationList = lazy(
+  () =>
+    import('../components/PaginationList/PaginationList'),
+);
+
+export default function MoviesPage() {
+  const history = useHistory();
+  const location = useLocation();
+  const searchURL = new URLSearchParams(location.search).get('query') ?? '';
+  const currentPage = new URLSearchParams(location.search).get('page') ?? 1;
+
+  const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(currentPage);
+  const [query, setQuery] = useState('');
+  const isFirstRender = useRef(true);
+  const [totalPages, setTotalPages] = useState(0);
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+
+    MoviesAPI.fetchMoviesBySearch(page, query).then(data => {
+      setMovies(data.results);
+      setTotalPages(data.total_pages);
+    });
+  }, [page, query]);
+
+  useEffect(() => {
+    if (searchURL === '') {
+      return;
+    }
+
+    setQuery(searchURL);
+  }, [searchURL]);
+
+  const setHistory = (query, value = 1) => {
+    history.push({ ...location, search: `query=${query}&page=${value}` });
+  };
+
+  const handleChange = (event, value) => {
+    setPage(value);
+    setHistory(query, value);
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  const handleFormSubmit = input => {
+    if (query === input) {
+      return;
+    }
+
+    setQuery(input);
+    setHistory(input);
+  };
+
+  return (
+    <div>
+      <SearchForm onSubmit={handleFormSubmit} />
+
+      <Suspense fallback={<Preloader />}>
+        <MoviesList movies={movies} url="movies/" />
+
+        <PaginationList
+          movies={movies}
+          totalPages={totalPages}
+          page={Number(page)}
+          handleChange={handleChange}
+        />
+      </Suspense>
+    </div>
+  );
+}
