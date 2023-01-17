d9e337fbf65bf8836153d10af3c76355a7542ab7
YurecUA <81627173+YurecUA@users.noreply.github.com>
2023-01-17 17:12:00 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation';
-import { lazy, Suspense } from 'react';
-import Container from './components/Container/Container';
-import Loader from 'components/Loader/Loader';
 import MovieCastView from 'pages/MovieCastView';
 import MovieReview from 'pages/MovieReview';
-
-const HomePage = lazy(() =>
-  import('./pages/HomePage' /* webpackChunkName:"HomePage" */)
-);
-const MoviesPage = lazy(() =>
-  import('./pages/MoviesPage' /* webpackChunkName:"MoviesPage" */)
-);
-const MovieDetailsPage = lazy(() =>
-  import(
-    './pages/MovieDetailsPage' /* webpackChunkName:"MovieDetailsPage" */
-  )
-);
-const NotFoundView = lazy(() =>
-  import('./pages/NotFoundView' /* webpackChunkName:"NotFoundView" */)
-);
+import HomePage from 'pages/HomePage';
+import MoviesPage from 'pages/MoviesPage';
+import MovieDetailsPage from 'pages/MovieDetailsPage';
 
 export const App = () => {
   return (
@@ -27,12 +12,11 @@ export const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<HomePage />} />
         <Route path="movies" element={<MoviesPage />} />
-        <Route path="movies/:movieId" element={<MovieDetailsPage />} >
-        <Route path="reviews" element={<MovieReview />} />
+        <Route path="movies/:movieId" element={<MovieDetailsPage />}>
+          <Route path="reviews" element={<MovieReview />} />
           <Route path="cast" element={<MovieCastView />} />
         </Route>
       </Route>
     </Routes>
   );
 };
-
